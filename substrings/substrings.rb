5e63ecbeed2c15f3848070f24f1cc07f18137447
Oleh <zaaqq135@gmail.com>
2024-02-16 21:55:20 +0200

    done substrings task

---
+++ substrings/substrings.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+def substrings(string, dict)
+  str = string.downcase
+
+  dict.each_with_object({}) do |word, res|
+    match = str.scan(word).length
+    res[word] = match if match.positive?
+    res
+  end
+end
+
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+
+# puts substrings('below', dictionary)
+# puts substrings("Howdy partner, sit down! How's it going?", dictionary)
