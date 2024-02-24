54da771fcbc6f7bbadd8cd39b352852a0f9447b9
Oleh <zaaqq135@gmail.com>
2024-02-24 16:55:25 +0200

    refactor TacToe module

--- tic-tac-toe/tic_tac_toe.rb
+++ tic-tac-toe/tic_tac_toe.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 module TicTacToe
-  # Class Board fot Tic-Tac-Toe
+  # Class Board for Tic-Tac-Toe
   class Board
+    attr_reader :lines
+
     def initialize
       @lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     end
@@ -15,52 +17,42 @@ module TicTacToe
       end
     end
 
-    def set_cill(cill, marker)
+    def set_cell(cell, marker)
       @lines.each do |line|
-        next unless line.include?(cill) && cill.is_a?(Integer)
+        next unless line.include?(cell) && cell.is_a?(Integer)
 
-        index = line.find_index(cill)
-        line[index] = marker
+        line[line.index(cell)] = marker
 
-        puts "Marker #{marker} is set to position #{cill}"
+        puts "Marker #{marker} is set to position #{cell}"
         return true
       end
       puts 'There is no such position. Choose another'
       false
     end
-
-    def lines
-      @lines.flatten
-    end
   end
 
   # Player class
   class Player
-    def initialize(player_name, player_marker)
-      @name = player_name
-      @marker = player_marker
-    end
-
     attr_reader :name, :marker
+
+    def initialize(name, marker)
+      @name = name
+      @marker = marker
+    end
   end
 
   # Class for game management
   class Game
     LINES = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6]
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
     ].freeze
 
     def initialize
       @board = Board.new
-      @player1 = Player.new(player_name, player_marker('X'))
-      @player2 = Player.new(player_name, player_marker('O'))
+      @player1 = Player.new(player_name('X'), 'X')
+      @player2 = Player.new(player_name('O'), 'O')
 
       puts 'Game Starts!'
     end
@@ -72,39 +64,31 @@ module TicTacToe
     end
 
     def set_marker(position, marker)
-      @board.set_cill position, marker
+      @board.set_cell(position, marker)
     end
 
     def win_match(marker)
-      LINES.each do |indexes|
-        values = indexes.map { |index| @board.lines[index] }
-        match = values.uniq
-        return true if (match.length == 1) && match.include?(marker)
+      LINES.any? do |indexes|
+        values = indexes.map { |index| @board.lines.flatten[index] }
+        values.uniq == [marker]
       end
-
-      false
     end
 
     def all_draw
-      @board.lines.none? { |value| value.is_a? Integer }
+      @board.lines.flatten.none? { |value| value.is_a? Integer }
     end
 
     private
 
-    def player_name
+    def player_name(marker)
       player_name = ''
 
       while player_name.empty?
-        puts 'Write first player name: '
+        puts "Write the name for the player with marker #{marker}:"
         player_name = gets.chomp.strip
       end
 
       player_name
-    end
-
-    def player_marker(marker)
-      puts "Your marker is #{marker}"
-      marker
     end
   end
 end
